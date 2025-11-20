27d20435f0bbba921b9747aa8b03c761c158b0ff
ChillTevin <169070677+ChillTevin@users.noreply.github.com>
2025-11-20 00:32:25 -0300

    Fix ngrok command to use local executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Iniciando entorno XRDP + ngrok..."
 xrdpArch start &
 
 # Exponer puerto 3389 con ngrok en región SA
-ngrok tcp 3389 --region=sa &
+./ngrok tcp 3389 --region=sa &
 
 # Mantener el contenedor vivo por 6 horas (21600 segundos)
 sleep 21600
